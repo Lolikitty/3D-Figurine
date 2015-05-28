ce94021483c0fdd7ab4998b302ba02322448b665
master <aaa1218bbb@yahoo.com.tw>
2015-05-28 10:40:06 +0800

    Update Page 1

--- 2. Unity Project/Assets/1. Project/2. Code/2. Private/ImageCrop/ImageCrop.cs
+++ 2. Unity Project/Assets/1. Project/2. Code/2. Private/ImageCrop/ImageCrop.cs
@@ -5,12 +5,17 @@ public class ImageCrop : MonoBehaviour {
 
 	public GameObject button_Next;
 
+	public ImageCropControl icc;
+
 	void Awake(){
 		UIEventListener.Get (button_Next).onClick = Button_Next;
+
+
 	}
 	
 	void Start () {
-	
+		icc.SetControlEnable (true);
+		icc.SetDepth (10);
 	}
 
 	void Update () {
